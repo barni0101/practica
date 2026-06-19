7e45ff07e53d86808a3d4d9967ce81d896085486
barni0101 <sodacall21@gmail.com>
2026-06-19 16:16:23 +0300

    Delete public directory

--- public/testdb.php
+++
@@ -1,9 +0,0 @@
-<?php
-$mysqli = new mysqli("localhost", "root", "", "ci4", 3307);
-
-if ($mysqli->connect_error) {
-    die("Connection failed: " . $mysqli->connect_error);
-}
-echo "Connected successfully!";
-$mysqli->close();
-?>
